27bd57b3ab2fcfca9282ba7dc45c7f821596df19
heeyomi <heeyomi@heeyomi>
2021-05-13 20:00:59 +0900

    Chapter08 solved Problem

---
+++ heekyoung/src/main/java/chaper08Prob/DaoExample.java
@@ -0,0 +1,17 @@
+package chaper08Prob;
+
+public class DaoExample {
+	
+	public static void doWork(DataAccessObject dao) {
+		dao.select();
+		dao.insert();
+		dao.update();
+		dao.delete();
+	}
+	
+	public static void main(String[] args) {
+		doWork(new OracleDao());
+		doWork(new MySqlDao());
+	}
+
+}
---
+++ heekyoung/src/main/java/chaper08Prob/Dog.java
@@ -0,0 +1,8 @@
+package chaper08Prob;
+
+public class Dog implements Soundable {
+	@Override
+	public String sound() {
+		return "멍멍";
+	}
+}
