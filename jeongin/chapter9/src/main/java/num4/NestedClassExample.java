ae210693c6caaf191619e43633ce7db6d530e598
ruby723 <molly723819@gmail.com>
2021-05-17 17:46:06 +0900

    chap9

---
+++ jeongin/chapter9/src/main/java/num4/NestedClassExample.java
@@ -0,0 +1,15 @@
+package num4;
+
+import num4.Car.Tire;
+
+public class NestedClassExample {
+
+	public static void main(String[] args) {
+		
+		Car myCar = new Car();
+		Car.Tire tire=myCar.new Tire();
+		Car.Engine engine = new Car.Engine();
+		
+	}
+
+}
---
+++ jeongin/chapter9/src/main/java/num5/AnonymousExample.java
@@ -0,0 +1,17 @@
+package num5;
+
+public class AnonymousExample {
+
+	public static void main(String[] args) {
+		Anonymous anony = new Anonymous();
+		anony.field.run();
+		anony.method1();
+		anony.method2(new Vehicle() {
+				public void run() {
+					System.out.println("트럭이 달립니다.");
+					}
+				}
+		);
+	}
+
+}
